9a9402491f3bcff98aa07023fa320e55ffc2b370
touya-01 <touya0205touya@gmail.com>
2025-07-17 16:57:38 +0900

    07-17授業内容

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Models\OrderDetail;
 use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class OrderController extends Controller
 {
@@ -29,25 +31,41 @@ class OrderController extends Controller
             ];
         }
 
-        $order = new Order;
-        $order->user_id = $request->user()->id;
-        $order->total_price = $totalPrice;
-        $order->save();
+        try {
+            DB::beginTransaction();
+            $order = new Order;
+            $order->user_id = $request->user()->id;
+            $order->total_price = $totalPrice;
+            $order->save();
 
-        foreach ($items as $item) {
-            $product = $item['product'];
-            $quantity = $item['quantity'];
+            foreach ($items as $item) {
+                $product = $item['product'];
+                $quantity = $item['quantity'];
 
-            $orderDetail = new OrderDetail;
-            $orderDetail->order_id = $order->id;
-            $orderDetail->product_id = $product->id;
-            $orderDetail->price = $product->price;
-            $orderDetail->quantity = $quantity;
-            $orderDetail->save();
+                $orderDetail = new OrderDetail;
+                $orderDetail->order_id = $order->id;
+                $orderDetail->product_id = $product->id;
+                $orderDetail->price = $product->price;
+                $orderDetail->quantity = $quantity;
+                $orderDetail->save();
+
+                $stock = $product->stock;
+                $stock -= $quantity;
+                if ($stock < 0) {
+                    throw new Exception('在庫がないです');
+                }
+                $product->stock = $stock;
+                $product->save();
+            }
+
+            session()->forget('cart');
+
+            echo '注文しました';
+            DB::commit();
+        } catch (Exception $e) {
+            echo 'エラーが起こりました<br>';
+            echo $e->getMessage();
+            DB::rollBack();
         }
-
-        session()->forget('cart');
-
-        echo '注文しました';
     }
 }
